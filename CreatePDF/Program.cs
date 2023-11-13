a670628014788028cd7877316fd6ef5de0ec6208
Felipe Gavilan <elmejor_887@hotmail.com>
2023-11-13 10:17:34 -0400

    Add project files.

---
+++ CreatePDF/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Previewer;
+
+QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
+
+//Document.Create(container =>
+//{
+//    container.Page(page =>
+//    {
+//        page.Size(PageSizes.A4);
+
+//        page.Margin(1,
+//            QuestPDF.Infrastructure.Unit.Centimetre);
+
+//        page.Header().Text("Hello world!")
+//        .Bold().FontSize(35)
+//        .FontColor(Colors.Red.Medium);
+
+//    });
+//}).GeneratePdf("hello world.pdf");
+
+Document.Create(container =>
+{
+    container.Page(page =>
+    {
+        page.Margin(1, QuestPDF.Infrastructure.Unit.Centimetre);
+
+        page.Header().Text("Let's learn .NET")
+        .Bold().FontSize(35).FontColor(Colors.Blue.Medium);
+
+        page.Content().Column(column =>
+        {
+            column.Spacing(20);
+
+            column.Item().Text(Placeholders.LoremIpsum());
+            column.Item().Image(Placeholders.Image(200, 100));
+
+            column.Item().Row(row =>
+            {
+                row.Spacing(20);
+
+                row.RelativeItem().Column(c =>
+                {
+                    c.Item().Text(Placeholders.LoremIpsum());
+                    c.Item().Image(Placeholders.Image(200, 100));
+                });
+
+                row.RelativeItem().Column(c =>
+                {
+                    c.Item().Text(Placeholders.LoremIpsum());
+                    c.Item().Image(Placeholders.Image(200, 100));
+                });
+            });
+
+            column.Item().Row(row =>
+            {
+                row.Spacing(20);
+
+                row.RelativeItem().Column(c =>
+                {
+                    c.Item().Text(Placeholders.LoremIpsum());
+                    c.Item().Image(Placeholders.Image(200, 100));
+                });
+
+                row.RelativeItem().Column(c =>
+                {
+                    c.Item().Text(Placeholders.LoremIpsum());
+                    c.Item().Image(Placeholders.Image(200, 100));
+                });
+
+                row.RelativeItem().Column(c =>
+                {
+                    c.Item().Text(Placeholders.LoremIpsum());
+                    c.Item().Image(Placeholders.Image(200, 100));
+                });
+            });
+        });
+
+        page.Footer().AlignCenter().Text(x =>
+        {
+            x.CurrentPageNumber();
+            x.Span(" / ");
+            x.TotalPages();
+        });
+
+    });
+}).ShowInPreviewer();
+
+Console.WriteLine("Hello, World!");
